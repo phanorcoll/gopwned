3a4cdcffa7352be1bf047928e4f76703c5cc65f3
Phanor Coll <phanorcoll@gmail.com>
2016-12-12 20:21:44 -0430

    Issue #3
    added the correct format documentation to the code

--- lib/emailValidation/validate.go
+++ lib/emailValidation/validate.go
@@ -2,6 +2,7 @@ package emailvalidation
 
 import "regexp"
 
+//Validates that the email provided has the correct format
 func Validate(e string) bool {
 	Re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	return Re.MatchString(e)
